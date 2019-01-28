5575da10779f70c22774afc4a4e0ada51f714226
LuisMiguelRodriguez <luis.the.coder@gmail.com>
2019-01-28 16:57:31 -0600

    Added Solved Folder

--- server/routes/git-route.js
+++ server/routes/git-route.js
@@ -14,28 +14,32 @@ module.exports = function (app) {
 
         console.log('req.body : ', req.body);
 
+        var classRepo = req.body.dir.split('01-Class-Content/')[1];
+
+        var finalDest = `${lessPlanPath}${classRepo}`;
+
         
 
         // console.log(__dirname+ '../utils-module')
         // shell.exec(__dirname + '../utils-module/bash/gitSolved.sh')
 
         
-        let testscript = exec( `${__dirname}/../utils-module/bash/gitSolved.sh ${lessPlanPath+req.body.dir}/  ${lessPlanPath}test-repo`)
+        let testscript = exec( `${__dirname}/../utils-module/bash/gitSolved.sh ${lessPlanPath+req.body.dir}/  ${lessPlanPath}test-repo/${finalDest}`)
 
         // console.log('directory path :  ', directoryPath)
         // const testscript = exec(`cp  -r ${lessPlanPath+req.body.dir}/. ${lessPlanPath}/test-repo/Solved `);
         
-        testscript.stdout.on('data', function(data){
-            console.log(data); 
-            // sendBackInfo();
+        // testscript.stdout.on('data', function(data){
+        //     console.log(data); 
+        //     // sendBackInfo();
 
 
-        });
+        // });
         
-        testscript.stderr.on('data', function(data){
-            console.log(data);
-            // triggerErrorStuff(); 
-        });
+        // testscript.stderr.on('data', function(data){
+        //     console.log(data);
+        //     // triggerErrorStuff(); 
+        // });
 
         res.sendStatus(200)
 
